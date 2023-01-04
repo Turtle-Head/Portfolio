e9b18ff6183d502ec7b6d92cc7c2362f2fd53604
James Fehr <Turtle-Head@users.noreply.github.com>
2023-01-04 13:57:07 -0800

    updates

--- js/script.js
+++ js/script.js
@@ -12,10 +12,28 @@ function ProjectViewModel() {
   var self = this;
 
   self.projects = ko.observableArray([
-    new Project('Bugged', 'http://turtle-head.github.io/Bugged/', 'img/Bugged.png', 'https://github.com/Turtle-Head/Bugged'),
-    new Project('Cat Clicker', 'http://turtle-head.github.io/CatClicker/', 'img/CatClicker.png', 'https://github.com/Turtle-Head/CatClicker'),
-    new Project('Gelp: Neighborhood Map Project', 'http://turtle-head.github.io/Project5/', 'img/gelp.jpg', 'https://github.com/Turtle-Head/Project5')//,
-    //new Project('Feed Reader: Testing', 'http://turtle-head.github.io/FeedReader-P6/', 'img/feedreader.png', 'https://github.com/Turtle-Head/FeedReader-P6')
+    new Project(
+      "Coding the little things",
+      "http://turtle-head.github.io/Coding-the-little-things/",
+      "img/code.png",
+      "http://turtle-head.github.io/Coding-the-little-things/"
+    ),
+    new Project(
+      "Bugged",
+      "http://turtle-head.github.io/Bugged/",
+      "img/Bugged.png",
+      "https://github.com/Turtle-Head/Bugged"
+    ),
+    new Project(
+      "Cat Clicker",
+      "http://turtle-head.github.io/CatClicker/",
+      "img/CatClicker.png",
+      "https://github.com/Turtle-Head/CatClicker"
+    ),
+    new Project("Gelp: Neighborhood Map Project", 
+    "http://turtle-head.github.io/Project5/", 
+    "img/gelp.jpg", 
+    "https://github.com/Turtle-Head/Project5")
   ]);
 }
 
