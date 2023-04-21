4f6e1b1155ecb120a7d033c70621781d2c193021
James Fehr <Turtle-Head@users.noreply.github.com>
2023-04-20 17:40:08 -0700

    Adding new project

--- script.js
+++ script.js
@@ -1,78 +1,22 @@
 // Class for project grid
-function Project (name, url, pic, git) {
-  var self = this
-  self.name = name
-  self.url = ko.observable(url)
-  self.pic = ko.observable(pic)
-  self.git = ko.observable(git)
+function Project(name, url, pic, git) {
+  var self = this;
+  self.name = name;
+  self.url = ko.observable(url);
+  self.pic = ko.observable(pic);
+  self.git = ko.observable(git);
 }
 
 // ViewModel for the screen
-function ProjectViewModel () {
-  var self = this
+function ProjectViewModel() {
+  var self = this;
 
   self.projects = ko.observableArray([
-    new Project(
-      'Coding the little things',
-      'http://turtle-head.github.io/Coding-the-little-things/',
-      'img/code.png',
-      'http://turtle-head.github.io/Coding-the-little-things/'
-    ),
-    new Project(
-      'Bugged',
-      'http://turtle-head.github.io/Bugged/',
-      'img/bugged-min.png',
-      'https://github.com/Turtle-Head/Bugged'
-    ),
-    new Project(
-      'Cat Clicker',
-      'http://turtle-head.github.io/CatClicker/',
-      'img/catClicker-min.png',
-      'https://github.com/Turtle-Head/CatClicker'
-    ),
-    new Project(
-      'Gelp',
-      'http://turtle-head.github.io/Project5/',
-      'img/gelp-min.png',
-      'https://github.com/Turtle-Head/Project5'
-    ),
-    new Project(
-      'Feed Reader',
-      'http://turtle-head.github.io/FeedReader-P6/',
-      'img/feedreader-min.png',
-      'https://github.com/Turtle-Head/FeedReader-P6'
-    ),
-    new Project(
-      'CleanUp',
-      'https://turtle-head.github.io/CleanUp/index.html',
-      'img/water-1761027.jpg',
-      'https://github.com/Turtle-Head/CleanUp'
-    ),
-    new Project(
-      'CalculatorV2',
-      'https://github.com/Turtle-Head/CalculatorV2',
-      'img/calcV2.png',
-      'https://github.com/Turtle-Head/CalculatorV2'
-    ),
-    new Project(
-      'Calculator-v6',
-      'https://github.com/Turtle-Head/Calculator-v6',
-      'img/CalcV6.png',
-      'https://github.com/Turtle-Head/Calculator-v6'
-    ),
-    new Project(
-      'AirBus',
-      'https://github.com/Turtle-Head/AirBub',
-      'img/AirBus.png',
-      'https://github.com/Turtle-Head/AirBub'
-    ),
-    new Project(
-      'BattleShip',
-      'https://github.com/Turtle-Head/BattleShip',
-      'img/BS3.png',
-      'https://github.com/Turtle-Head/BattleShip'
-    )
-  ])
+    new Project('Bugged', 'http://turtle-head.github.io/Bugged/', 'img/Bugged.png', 'https://github.com/Turtle-Head/Bugged'),
+    new Project('Cat Clicker', 'http://turtle-head.github.io/CatClicker/', 'img/CatClicker.png', 'https://github.com/Turtle-Head/CatClicker'),
+    new Project('Gelp: Neighborhood Map Project', 'http://turtle-head.github.io/Project5/', 'img/gelp.jpg', 'https://github.com/Turtle-Head/Project5'),
+    new Project('Feed Reader: Testing', 'http://turtle-head.github.io/FeedReader-P6/', 'img/feedreader.png', 'https://github.com/Turtle-Head/FeedReader-P6')
+  ]);
 }
 
-ko.applyBindings(new ProjectViewModel())
+ko.applyBindings(new ProjectViewModel());
